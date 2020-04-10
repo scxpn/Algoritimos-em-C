87eaa34fba082b1fa1dece4b93075393d38c1146
ScorpionWT <ubirajara2018@gmail.com>
2020-04-10 11:34:27 -0300

    Reconfiguração do código

--- Atividades/Atividade-02NumerosPrimos.cpp
+++ Atividades/Atividade-02NumerosPrimos.cpp
@@ -1,14 +1,15 @@
 #include<stdio.h>
 
 main(){
-	printf("Esses sao os numeros primos:\n ");
-	int numberP;
-	
-	while(numberP < 100){
-		numberP = numberP + 1;
-		if (numberP % 1 == 1){
-			printf("\n%d", numberP);
-		}		
-	}
+    printf("A seguir os nuneros primos entre 1 e 100");
+    int primos;
+    primos=1;
+     printf("2\n3\n5\n7");
+      while(primos<100){
+     primos=primos+1;
+     if(primos %2 != 0 && primos %3 != 0 && primos %5 != 0 && primos %7 != 0){
+       printf("\n%d",primos);
+        }
+    }
 }
 
