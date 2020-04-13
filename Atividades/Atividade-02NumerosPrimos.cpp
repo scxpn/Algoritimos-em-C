40f05e4cd36338c385c84c982cfd6b62373e29fc
ScorpionWT <ubirajara2018@gmail.com>
2020-04-13 00:10:57 -0300

    Versão final

--- Atividades/Atividade-02NumerosPrimos.cpp
+++ Atividades/Atividade-02NumerosPrimos.cpp
@@ -1,33 +1,36 @@
 #include<stdio.h>
 //Código baseado no Evellyne Anunciada
 
-main(){
+int main(){
+	
+	//Declaração de Variaveis
+	int a=2,b=2,c=0;
+	
 	//Apresentação da função do código
     printf("Esses sao os numeros primos entre 1 e 100: ");
     
-    //Declaração das variaveis
-    int counter, c, b= 2, a = 2;
-    
-    //Looping principal da execução
-    while( a < 100){
-    	
-    	while(b < a){
-    		if(a % b == 0){
-    			c = 1;
-    			
+    //Contador até o 100
+	while(a<=100){
+		//Verificação de numeros
+		while(b<a){
+			if(a%b==0){
+				
+				c++;
+				break;
 			}
-    		b++;
+		b++;
 		}
 		
-		b = 2;
+		b=2;
 		
-		if(c == 0){
-			printf("\n%d", a);
-			counter++;
+		if(c==0){
+		printf("\n %d",a);
+		
 		}
-		c =0;
+		c=0;
 		a++;
-		
-	}
+	
+	}		
 }
+
 
